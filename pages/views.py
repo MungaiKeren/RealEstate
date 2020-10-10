9cc1a73fcd7be77cbc665ff8d179ae37f9da6337
MungaiKeren <wambukeren@gmail.com>
2020-10-10 14:46:35 +0300

    Create base templates

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is the home page")
+    return render(request, "pages/index.html")
+
+def about(request):
+    return render(request, 'pages/about.html')
