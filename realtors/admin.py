9ccf8040e51a95a1ed4d9565fe45cadd4af1ca29
MungaiKeren <wambukeren@gmail.com>
2020-11-16 14:20:29 +0300

    customize the realtors model

--- realtors/admin.py
+++ realtors/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Realtor
 
 # Register your models here.
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', "name", 'phone', 'email','is_mvp', 'hire_date')
+    list_display_links = ('id', 'name')
+    list_filter = ('hire_date','is_mvp')
+    search_fields = ('id','name')
+    list_editable = ("is_mvp",)
+    list_per_page = 25
+
+admin.site.register(Realtor, RealtorAdmin)
