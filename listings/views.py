a61ea6275a16ec1df246d9746c59c5bee476047a
MungaiKeren <wambukeren@gmail.com>
2020-11-17 10:33:50 +0300

    worked on pagination

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def listings(request):
     listings = Listing.objects.all()
+    paginator = Paginator(listings, 3) # shows 3 listings per page
+
+    page_number = request.GET.get('page')
+    paged_listings = paginator.get_page(page_number)
 
     context = {
-        'listings': listings,
+        'listings': paged_listings,
     }
     return render(request, 'listings/listings.html', context)
 
