4aae57132981e1f4a028707acfc8d8cee67c1e3a
MungaiKeren <wambukeren@gmail.com>
2020-11-19 17:24:54 +0300

    populate the search view

--- listings/views.py
+++ listings/views.py
@@ -26,9 +26,50 @@ def single_listing(request, listing_id):
 
 def search(request):
 
+    qs_list = Listing.objects.order_by('-list_date')
+    # keywords
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            qs_list = qs_list.filter(description__icontains=keywords)
+    
+    #city
+    
+    if 'city' in request.GET:
+        city = request.GET['city']
+
+        if city:
+            qs_list = qs_list.filter(city__iexact=city)
+    
+    # county
+
+    if 'county' in request.GET:
+        county = request.GET['county']
+        if county:
+            qs_list = qs_list.filter(county__iexact=county)
+    
+    # bedrooms
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+
+        if bedrooms:
+            qs_list = qs_list.filter(bedrooms__lte=bedrooms)
+    
+    # prices
+    if 'price' in request.GET:
+        price = request.GET['price']
+
+        if price:
+            qs_list = qs_list.filter(price__lte=price)
+
+
+
     context = {
         'county_choices': county_choices,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
+        'listings': qs_list,
     }
     return render(request, 'listings/search.html', context)
